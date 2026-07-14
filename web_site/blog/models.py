3d1e75c8738d5a92192ff4ef43de5e8f4b64492d
Kopesh <kopesh@example.com>
2026-07-14 23:25:25 +0530

    DB configured

--- web_site/blog/models.py
+++ web_site/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class posts(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=500)
+    image=models.ImageField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+
 # Create your models here.
--- web_site/web_site/settings.py
+++ web_site/web_site/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'web_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'last_recall',
+        'USER':'root',
+        'PASSWORD':'kopeshdb',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
