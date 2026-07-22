afd941ec2bf9916f6102e4d2424c3b5cdbb363c6
Kopesh <kopesh@example.com>
2026-07-23 00:01:25 +0530

    implemented for showing old messages and field error handling

--- web_site/blog/views.py
+++ web_site/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import posts
 from django.core.paginator import Paginator
+from .forms import contactform
 
 
 
@@ -24,7 +25,21 @@ def detail(request,sl):
         post=None
     return render(request,"detail.html",{'post':post,'related':rel})
 
+
+
 def contact(request):
+    
+    form = contactform()
+    if(request.method == "POST"):
+        form = contactform(request.POST)
+        name = request.POST["name"]
+        email = request.POST["email"]
+        mess = request.POST["message"]
+        if form.is_valid():
+            print(form.cleaned_data["name"])
+        else:
+            return render(request,"contact.html",{'form':form,'name':name,'email':email,'mess':mess})
+        
     return render(request,"contact.html")
 
 # Create your views here.
