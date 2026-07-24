d8292b47f7cc791cfc9cf768aa89361cc0b676a7
Kopesh <kopesh@example.com>
2026-07-24 18:58:47 +0530

    mostly Done

--- web_site/blog/admin.py
+++ web_site/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import cat,posts,about
+from .models import cat,posts,about,user_data
 
 
 
@@ -8,8 +8,18 @@ class postsAdmin(admin.ModelAdmin):
     search_fields = ('title','content','posts__cato')
     list_filter = ('cato','data')
 
-admin.site.register(cat)
+
+class catAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class user_dataAdmin(admin.ModelAdmin):
+    list_display = ('name','email')
+    search_fields = ('name','email','message')
+
+admin.site.register(cat,catAdmin)
 admin.site.register(posts,postsAdmin)
 admin.site.register(about)
+admin.site.register(user_data,user_dataAdmin)
 
 # Register your models here.
