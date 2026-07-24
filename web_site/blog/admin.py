ebe606bae305d5f77f8d75449a2ac2d98380f03b
Kopesh <kopesh@example.com>
2026-07-24 18:42:35 +0530

    admin interface chnaged

--- web_site/blog/admin.py
+++ web_site/blog/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import cat,posts,about
 
 
+
+class postsAdmin(admin.ModelAdmin):
+    list_display = ('title','content','cato')
+    search_fields = ('title','content','posts__cato')
+    list_filter = ('cato','data')
+
 admin.site.register(cat)
-admin.site.register(posts)
+admin.site.register(posts,postsAdmin)
 admin.site.register(about)
 
 # Register your models here.
