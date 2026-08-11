0ecc745735fdb8fc165c1b8d36f0efd41f1eb001
Kopesh <kopesh@example.com>
2026-08-11 23:22:57 +0530

    redirect is ready for authenticated user

---
+++ web_site/web_site/middleware.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from django.shortcuts import render,redirect
+
+
+class auth_redirect:
+    def __init__(self,get_response):
+        self.get_response = get_response
+    
+    def __call__(self,request):
+        if request.user.is_authenticated:
+            lis_path = [reverse("blog:login"),reverse("blog:register")]
+            if request.path in lis_path:
+                return redirect(reverse("blog:index"))
+        res = self.get_response(request)
+        return res
+
+
--- web_site/web_site/settings.py
+++ web_site/web_site/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'web_site.middleware.auth_redirect',
 ]
 
 ROOT_URLCONF = 'web_site.urls'
